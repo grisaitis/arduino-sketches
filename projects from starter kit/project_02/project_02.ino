96792300489cc928e79ec80cf52e629de15a58eb
William Grisaitis <wgrisaitis@gmail.com>
2015-06-02 23:29:11 -0400

    adding set up

--- projects from starter kit/project_02/project_02.ino
+++ projects from starter kit/project_02/project_02.ino
@@ -5,6 +5,11 @@ int switchState = 0;
 
 // setup() and void() must be defined in any sketch, it seems. 
 void setup() {
+  pinMode(2,INPUT); // this is the switch pin
+  
+  pinMode(3,OUTPUT); // for green LED
+  pinMode(4,OUTPUT); // for red LED
+  pinMode(5,OUTPUT); // for another red LED
 }
 
 void loop(){
