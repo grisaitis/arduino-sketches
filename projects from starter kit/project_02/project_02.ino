0421a10d7f95ca34f19e7755d7171eda308fbc63
William Grisaitis <wgrisaitis@gmail.com>
2015-06-03 00:48:14 -0400

    first working version!

--- projects from starter kit/project_02/project_02.ino
+++ projects from starter kit/project_02/project_02.ino
@@ -13,4 +13,28 @@ void setup() {
 }
 
 void loop(){
+  // this checks the voltage on pin 2
+  switchState = digitalRead(2); 
+  // if voltage exists, then digitalRead returns 1 or the value 'HIGH'
+  // otherwise, it returns 0 or 'LOW'
+  if (switchState == LOW) {
+    // the button is not pressed
+    // send 5V of voltage out through pin 3
+    digitalWrite(3,HIGH); // green LED
+    // send zero volts out pins 4 and 5
+    digitalWrite(4,LOW); // red LED
+    digitalWrite(5,LOW); // red LED
+  }
+  else {
+    // the button is pressed
+    // turn off the 
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,HIGH);
+    delay(250);
+    // toggle the LEDs
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(250);
+  }
 }
