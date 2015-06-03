835fd93d32d5467feb8b37cd0fe30f0bbcd96ef7
William Grisaitis <wgrisaitis@gmail.com>
2015-06-02 21:58:01 -0400

    adding starter kit project code (currently only project 2)

---
+++ projects from starter kit/project_02/project_02.ino
@@ -0,0 +1,11 @@
+// stores status of the switch on the circuit
+// the switch is manually changeable
+int switchState = 0; 
+
+
+// setup() and void() must be defined in any sketch, it seems. 
+void setup() {
+}
+
+void loop(){
+}
